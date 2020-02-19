5e491ec9027b1b5046290e8c2cce2b01722bb6d7
khairul Islam <khairul@orangetoolz.com>
2020-02-19 14:47:13 +0600

    easy configure for js & sass

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: ['./src/app.js', './src/css/main.scss'],
+    entry: ['./src/app.js'],
 
     output: {
         path: path.join(__dirname, 'public'),
